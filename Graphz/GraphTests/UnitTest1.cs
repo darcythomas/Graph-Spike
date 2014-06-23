15aa4fb777cc25090c78768ff691a61d066cd119
darcythomas <github@darcy.io>
2014-06-23 15:54:25 +1200

    Basic project with basic test

---
+++ Graphz/GraphTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GraphTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTheTester()
+        {
+            Assert.IsTrue(1==1);
+        }
+    }
+}
